b7dd14fa923104009192223e532cb2670a290f72
SocheatMa <masocheat168@gmail.com>
2022-08-19 14:30:03 +0700

    first commit

--- resources/views/backend/layouts/partials/logout.blade.php
+++ resources/views/backend/layouts/partials/logout.blade.php
@@ -4,9 +4,14 @@
     {{ Auth::guard('admin')->user()->name }}
     <i class="fa fa-angle-down"></i></h4>
     <div class="dropdown-menu">
-        <a class="dropdown-item" href="{{ route('admin.logout.submit') }}"
-        onclick="event.preventDefault();
-                      document.getElementById('admin-logout-form').submit();">Log Out</a>
+        <a class="dropdown-item" href="{{ route('admin.logout.submit') }}" onclick="event.preventDefault(); document.getElementById('admin-logout-form').submit();">
+            <i class="ti-power-off"></i>
+            <span class="ml-2">Log Out</span>
+        </a>
+        <a class="dropdown-item" href="{{ route('admin.logout.submit') }}"">
+            <i class="ti-power-off"></i>
+            <span class="ml-2">Log Out</span>
+        </a>
     </div>
 
     <form id="admin-logout-form" action="{{ route('admin.logout.submit') }}" method="POST" style="display: none;">
